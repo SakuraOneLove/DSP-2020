f3ea364509bfe7ac0a43f270600d91da8a402382
Oleg <apramov@mail.ru>
2020-05-07 17:10:11 +0300

    Update readme

--- nrgb2hex.rb
+++ nrgb2hex.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/ruby
 
-rgb = ARGV.map { |x| (x.to_f * 255).round.to_s(16) }
-puts('#' + rgb.join('').upcase)
+nums = ARGV.select { |x| x.match /\d+/}
+flag = ARGV - nums
+puts '#' + nums.map { |x| (x.to_f * 255).round.to_s(16) }.join('').upcase if (flag.empty? || (flag[0] == "-h" unless flag.empty?)) && !nums.empty?
+puts nums.map { |x| (x.to_f * 255).round.to_s }.join(' ').upcase unless flag.empty? && nums.empty && (flag[0] == "-d" unless flag.empty?)
